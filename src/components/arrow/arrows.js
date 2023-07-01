61079a22e6d44f183b7ff4cf81d00b01a6c96e58
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-07-01 12:38:42 -0500

    update state

--- src/components/arrow/arrows.js
+++ src/components/arrow/arrows.js
@@ -5,7 +5,7 @@ import style from 'aditional/toogle.module.css';
 import './arrow.css';
 
 function Dark() {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState('light');
   const toggleTheme = () => {
     if (theme === 'light') {
       setTheme('dark');
